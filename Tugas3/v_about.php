62419630508885e5238ae1dd29e583854a12f591
riadhiraznan <raznanriadhi776@gmail.com>
2023-10-27 14:14:55 +0700

    Tugas 3

---
+++ Tugas3/v_about.php
@@ -0,0 +1,22 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Riadhi</li> 
+        <li>Nama Belakang : Raznan</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Butuh rt 03 rw 06 Mojosongo, Mojosongo, Boyolali</li> 
+    </ul> 
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Jakarta</li> 
+    </ul> 
+    
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Badminton</li> 
+    </ul> 
+</section>
